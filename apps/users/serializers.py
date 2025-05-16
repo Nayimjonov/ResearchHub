e55184dff519ad3a429d8398a57631fa31a2182a
Samandar <samandarnayimjonov@gmail.com>
2025-05-16 16:57:08 +0500

    register serialzier created

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,1 +1,23 @@
+from rest_framework import serializers
 
+
+class UserRegisterSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    full_name = serializers.CharField(read_only=True)
+    institution = serializers.CharField()
+    department = serializers.CharField()
+    position = serializers.CharField()
+    orcid_id = serializers.CharField()
+    is_active = serializers.BooleanField(read_only=True)
+    is_staff = serializers.BooleanField(read_only=True)
+    is_verified = serializers.BooleanField(read_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
+    role = serializers.CharField(read_only=True)
+    citation_count = serializers.IntegerField(read_only=True)
+    h_index = serializers.IntegerField(read_only=True)
+    profile_url = serializers.URLField(read_only=True)
