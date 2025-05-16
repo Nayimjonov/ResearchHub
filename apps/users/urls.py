068630ab860d74eebc426480e9ad1b780e6524c9
Samandar <samandarnayimjonov@gmail.com>
2025-05-16 17:03:20 +0500

    user register url sozlandi

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        "auth/register/",
+        views.UserRegisterView.as_view(),
+        name="user-register",
+    )
+]
