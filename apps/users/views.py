ffe5307270fdb5a59fa3ef25f3682f8bb09cd756
Samandar <samandarnayimjonov@gmail.com>
2025-05-16 17:01:18 +0500

    user register view yozildi

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+
+from apps.users.serializers import UserRegisterSerializer
+
+User = get_user_model()
+
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
